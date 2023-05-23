fbc50ed1b5a24e47584bab61d5587d735240694b
amrutprabhu <amrutprabhu42@gmail.com>
2023-05-23 20:58:14 +0200

    Updated websocket client connection.

--- Home-Assistant/src/main/java/com/amrut/prabhu/ha/HAConfig.java
+++ Home-Assistant/src/main/java/com/amrut/prabhu/ha/HAConfig.java
@@ -3,16 +3,20 @@ package com.amrut.prabhu.ha;
 import com.amrut.prabhu.ha.homeassistant.SocketHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.client.WebSocketClient;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
+
+import java.util.concurrent.ExecutionException;
 
 @Configuration
 public class HAConfig {
 
     @Bean
-    public WebSocketClient webSocketClient(final SocketHandler socketHandler, HAConfigProperties haConfigProperties) {
+    public WebSocketSession webSocketSession(final SocketHandler socketHandler, HAConfigProperties haConfigProperties) throws ExecutionException, InterruptedException {
         WebSocketClient client = new StandardWebSocketClient();
-        client.execute(socketHandler, haConfigProperties.getUrl());
-        return client;
+
+        return client.execute(socketHandler, haConfigProperties.getUrl())
+                .get();
     }
 }
